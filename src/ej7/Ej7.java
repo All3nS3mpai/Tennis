16ac8c85c4972c1f77046be8f6ea2e11e5fafc4a
All3nS3mpai <All3nS3mpai@users.noreply.github.com>
2018-02-24 09:49:41 -0700

    Add files via upload

---
+++ src/ej7/Ej7.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej7;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Ej7 extends JPanel {
+
+	Ball ball = new Ball(this);
+	Racquet racquet = new Racquet(this);
+
+	public Ej7() {
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				racquet.keyReleased(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				racquet.keyPressed(e);
+			}
+		});
+		setFocusable(true);
+	}
+	
+	private void move() {
+		ball.move();
+		racquet.move();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		ball.paint(g2d);
+		racquet.paint(g2d);
+	}
+	
+	public void gameOver() {
+		JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+		System.exit(ABORT);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		JFrame frame = new JFrame("Mini Tennis");
+		Ej7 game = new Ej7();
+		frame.add(game);
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while (true) {
+			game.move();
+			game.repaint();
+			Thread.sleep(2);
+		}
+	}
+}
